424a5668aa213d85d6ac7d10095cdecad94a14fc
Hack Reactor Students <accounts@hackreactor.com>
2014-05-08 16:47:33 -0700

    Serve pages

--- helpers/post-helper.js
+++ helpers/post-helper.js
@@ -6,19 +6,20 @@ var serveAssets = require('../web/http-helpers').serveAssets;
 module.exports = function(req, res, url){
   archive.isUrlInList(url, function(isInList) {
     if(isInList) {
-      archive.isURLArchived(url, function(isArchived){
+      archive.isUrlArchived(url, function(isArchived){
         if(isArchived) {
           console.log('in list and archived');
-          serveAssets(res, archive.paths.archivedSites + '/' + url)
+          serveAssets(res, archive.paths.archivedSites + '/' + url);
         } else {
           console.log('in list but not archived');
-          // serve loading page
+          console.log(archive.paths.siteAssets + '/loading.html');
+          serveAssets(res, archive.paths.siteAssets + '/loading.html');
         }
       });
     } else {
       archive.addUrlToList(url, function(url){
         console.log('adding', url, 'to archive list');
-        // serve loading page
+        serveAssets(res, archive.paths.siteAssets + '/loading.html');
       });
     }
   });

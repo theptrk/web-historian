95f91696fd9d66c2e7da456f995b5e9b62efd41e
Hack Reactor Students <accounts@hackreactor.com>
2014-05-07 20:30:00 -0700

    Build infrastructure for GET requests and serving files

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,8 +11,15 @@ exports.headers = headers = {
 };
 
 exports.serveAssets = function(res, asset) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  var data = '';
+  fs.readFile(asset, function(err, partial){
+    if(err) {
+      console.error('err@@@!!!');
+      console.error(err);
+    }
+    data += partial;
+    res.end(data);
+  });
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,29 @@
+var fs = require('fs');
 var path = require('path');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var serveAssets = require('./http-helpers').serveAssets
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var method = req.method;
+  var pathname = url.parse(req.url).pathname;
+
+  console.log('serving', method, 'for', pathname);
+
+  if(pathname === '/') {
+    console.log('pathname is',pathname)
+    serveAssets(res, archive.paths.siteAssets + '/index.html');
+    //res.end(archive.paths.siteAssets + '/index.html');
+  }
+  else {
+    // default, if no predetermined path
+    serveAssets(res, archive.paths.siteAssets + pathname);
+  }
+
+
 };
+
+// 'siteAssets' : path.join(__dirname, '../web/public'),
+// 'archivedSites' : path.join(__dirname, '../archives/sites'),
+// 'list' : path.join(__dirname, '../archives/sites.txt')

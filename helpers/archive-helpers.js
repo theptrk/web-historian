67e508eec536c9a8321b4bbeea94a2393dd8586b
Hack Reactor Students <accounts@hackreactor.com>
2014-05-07 23:06:42 -0700

    Refactor to more closely follow async design pattern

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var exports = exports;
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -26,61 +27,45 @@ exports.initialize = function(pathsObj){
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback){
-  var self = this;
-  var listOfUrls = [];
-  function readLines(input, func) {
-    var remaining = '';
-
-    input.on('data', function(data) {
-      remaining += data;
-      var index = remaining.indexOf('\n');
-      var last  = 0;
-      while (index > -1) {
-        var line = remaining.substring(last, index);
-        last = index + 1;
-        func(line);
-        index = remaining.indexOf('\n', last);
-      }
-
-      remaining = remaining.substring(last);
-    });
-
-    input.on('end', function() {
-      if (remaining.length > 0) {
-        func(remaining);
-      }
-      callback(listOfUrls);
-    });
-  }
-
-  function func(data) {
-    listOfUrls.push(data);
-  }
-
-  var input = fs.createReadStream(self.paths.list);
-  readLines(input, func);
-};
-
-exports.isUrlInList = function(url){
-  var self = this;
-
-  var findUrl = function(array) {
-    if(array.indexOf(url) === -1) {
-      console.log('F')
-      return false;
+  fs.readFile(exports.paths.list, function(err, data){
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(data.toString().split('\n'));
+      callback(data.toString().split('\n'));
     }
-    console.log('T')
-    return true;
-  };
-
-  self.readListOfUrls(findUrl);
+  });
 };
 
-exports.addUrlToList = function(){
+exports.isUrlInList = function(url, callback){
+  exports.readListOfUrls(function(array){
+    callback(array.indexOf(url) !== -1);
+  });
 };
 
-exports.isURLArchived = function(){
+exports.addUrlToList = function(url){
+  var text = '\n' + url;
+  fs.appendFile(exports.paths.list, text, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('The "data to append" was appended to file!');
+  });
+};
+
+exports.isURLArchived = function(url, callback){
+  var self = this;
+  fs.exists(this.paths.archivedSites + '/' + url, function(exists) {
+    if(exists) {
+      console.log('isURLArchive says it exists')
+    } else {
+      console.log('isURLArchive says it doesnt exist - callback is commented')
+      //callback(url);
+    }
+  });
 };
 
 exports.downloadUrls = function(){
+  var self = this;
+  // chron job, regular job
 };
